efea02901da35c598b077accb500c18c3ca71f19
Rulin Tang <smiletrl@outlook.com>
2024-06-16 12:49:21 +0800

    fix test

--- medium/count-pairs-that-form-a-complete-day-ii/s_test.go
+++ medium/count-pairs-that-form-a-complete-day-ii/s_test.go
@@ -10,18 +10,18 @@ func TestCountCompleteDayPairs(t *testing.T) {
 	cases := []struct {
 		name   string
 		hours  []int
-		expect int
+		expect int64
 	}{
-		// {
-		// 	name:   "case 1",
-		// 	hours:  []int{12, 12, 30, 24, 24},
-		// 	expect: 2,
-		// },
-		// {
-		// 	name:   "case 2",
-		// 	hours:  []int{72, 48, 24, 3},
-		// 	expect: 3,
-		// },
+		{
+			name:   "case 1",
+			hours:  []int{12, 12, 30, 24, 24},
+			expect: 2,
+		},
+		{
+			name:   "case 2",
+			hours:  []int{72, 48, 24, 3},
+			expect: 3,
+		},
 		{
 			name:   "case 3",
 			hours:  []int{11, 22, 2, 25, 19, 2},
